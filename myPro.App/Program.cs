acc64cc11805d169a4d68d5e2fe53fae6c611016
happyonee <happyonee@163.com>
2020-11-29 08:02:46 +0800

    修改

--- myPro.App/Program.cs
+++ myPro.App/Program.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using StackExchange.Redis;
+using Newtonsoft.Json;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace myPro.App
 {
@@ -7,6 +11,18 @@ namespace myPro.App
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+            ConnectionMultiplexer redis = ConnectionMultiplexer.Connect("192.168.202.129:6379");
+
+            IDatabase db = redis.GetDatabase();
+
+            var batch = db.CreateBatch();
+            Task t1 = batch.StringSetAsync("name", "bob");
+            Task t2 = batch.StringSetAsync("age", 100);
+            batch.Execute();
+            Task.WaitAll(t1, t2);
+            Console.WriteLine("Age:" + db.StringGet("age"));
+            Console.WriteLine("Name:" + db.StringGet("name"));
         }
     }
 }
